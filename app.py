105676a0490047370a32e82df56b56d79428e460
nsravani3109 <90580467+nsravani3109@users.noreply.github.com>
2025-08-20 18:48:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/validate_mc/{mcid}")
+@app.get("/validate_mc")
 def validate_mc(mcid: str):
     if mcid.isdigit():
         return {"mcid": mcid, "valid": True}
